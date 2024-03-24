d7bcd3f80f5fff2f821f0af5449ad57cd84a7d42
0xyshv <writetoyshv@gmail.com>
2024-03-24 15:59:40 +0530

    chore: add frame layout with frames.js

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import { fetchMetadata } from "frames.js/next";
+
+
+export async function generateMetadata() {
+  return {
+    title: "My Page",
+    version: "vNext",
+    // provide a full URL to your /frames endpoint
+    other: await fetchMetadata(
+      new URL(
+        "/frames",
+        process.env.VERCEL_URL
+          ? `https://knowledge-cards-two.vercel.app/`
+          : "http://localhost:3000"
+      )
+    ),
+  };
+}
+
+export default function Page() {
+
+  return <span>My existing page</span>;
+}
